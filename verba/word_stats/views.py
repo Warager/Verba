79e2ed49f57ebc9d19f266acdd0e27eff7d6297c
Fomin Dmitry <fomin.dritmy@gmail.com>
2014-09-16 23:13:36 +0400

    Revert "Access error on hidden pages fixed"
    This reverts commit 3fa30d7cdf914848ceb819838f84b02b78f90b5f.

--- verba/word_stats/views.py
+++ verba/word_stats/views.py
@@ -25,70 +25,67 @@ def process(request):
     :param request:
     :return:
     """
-    if User.DoesNotExist:
-        return redirect("/")
-    else:
-        #This part used to import text and extra check parameters and assign them to variables
-        text = request.POST.get('text', "")
-        threeDigits = request.POST.get('threeDigits') == 'checked'
-        onlyRoot = request.POST.get('onlyRoot') == 'checked'
+    #This part used to import text and extra check parameters and assign them to variables
+    text = request.POST.get('text', "")
+    threeDigits = request.POST.get('threeDigits') == 'checked'
+    onlyRoot = request.POST.get('onlyRoot') == 'checked'
 
-        # This part of code counts through punctuation symbols finds them in text and replaces if find on .
-        for c in string.punctuation + " " + "\n":
-            text = text.replace(c, ".")
+    # This part of code counts through punctuation symbols finds them in text and replaces if find on .
+    for c in string.punctuation + " " + "\n":
+        text = text.replace(c, ".")
 
-        # This part of code makes all symbols in text lower case, removes blank lines before and after text and makes
-        # list of words, dividing them by .
-        text = text.lower().strip().split(".")
-        text = filter(None, text)
+    # This part of code makes all symbols in text lower case, removes blank lines before and after text and makes
+    # list of words, dividing them by .
+    text = text.lower().strip().split(".")
+    text = filter(None, text)
 
-        #This part count through the list of words and check if there any digits. Then it makes new list without digits
-        no_nums = []
-        for w in text:
-            try:
-                type(int(w)) == int
-            except ValueError:
-                no_nums.append(w)
-        text = no_nums
+    #This part count through the list of words and check if there any digits. Then it makes new list without digits
+    no_nums = []
+    for w in text:
+        try:
+            type(int(w)) == int
+        except ValueError:
+            no_nums.append(w)
+    text = no_nums
 
-        known_words = set()
-        for user_dictionary in UserDictionary.objects.filter(user=request.user):
-            known_words.add(user_dictionary.word)
+    known_words = set()
+    for user_dictionary in UserDictionary.objects.filter(user=request.user):
+        known_words.add(user_dictionary.word)
 
-        known_in_text = []
-        cnt = Counter()
-        for word in text:
-            word.strip()
-            # for s in string.punctuation+" ":
-            #     word = word.replace(s, " ")
-            if not word or word == " ":
-                continue
-            elif len(word) == 1:
-                continue
-            elif threeDigits and len(word) <= 2:
-                continue
-            elif onlyRoot:
-                word = stem(word)
-            try:
-                UserDictionary.objects.get(user=request.user, word=word)
-                known_in_text.append(word)
-            except UserDictionary.DoesNotExist:
-                cnt[word] += 1
-            # cnt[word] += 1
-        a = cnt.items()
-        # a.sort(key=lambda x: x[1], reverse=True)
-        data = {
-            "words": sorted(a),
-            "known_words": sorted(known_words),
-            "known_in_text": sorted(known_in_text)
-        }
-        return render(request, 'word_stats/analysis.html', data)
+    known_in_text = []
+    cnt = Counter()
+    for word in text:
+        word.strip()
+        # for s in string.punctuation+" ":
+        #     word = word.replace(s, " ")
+        if not word or word == " ":
+            continue
+        elif len(word) == 1:
+            continue
+        elif threeDigits and len(word) <= 2:
+            continue
+        elif onlyRoot:
+            word = stem(word)
+        try:
+            UserDictionary.objects.get(user=request.user, word=word)
+            known_in_text.append(word)
+        except UserDictionary.DoesNotExist:
+            cnt[word] += 1
+        # cnt[word] += 1
+    a = cnt.items()
+    # a.sort(key=lambda x: x[1], reverse=True)
+    data = {
+        "words": sorted(a),
+        "known_words": sorted(known_words),
+        "known_in_text": sorted(known_in_text)
+    }
+    return render(request, 'word_stats/analysis.html', data)
 
 
 def signup(request):
-    my_email = request.POST.get('login', "")
-    my_password = request.POST.get('password', "")
-    my_pass_conf = request.POST.get('confirm', "")
+    my_email = request.POST['login']
+    my_password = request.POST['password']
+    my_pass_conf = request.POST['confirm']
 
     sg = sendgrid.SendGridClient('warager', 'cfyahfywbcrj!')
 
@@ -113,8 +110,8 @@ def signup(request):
 
 
 def accounts(request):
-    my_email = request.POST.get('login', "")
-    my_password = request.POST.get('password', "")
+    my_email = request.POST['login']
+    my_password = request.POST['password']
 
     try:
         user = User.objects.get(email__iexact=my_email, is_active=True)
@@ -150,48 +147,46 @@ def rem_word(request):
 
 
 def my_dictionary(request):
-    if User.DoesNotExist:
-        return redirect("/")
-    else:
-        if request.method == "POST":
-            n_words = request.POST.get('words', "")
 
-            for c in string.punctuation + " " + "\n":
-                n_words = n_words.replace(c, ".")
+    if request.method == "POST":
+        n_words = request.POST.get('words', False)
 
-            n_words = n_words.lower().strip().split(".")
-            n_words = filter(None, n_words)
+        for c in string.punctuation + " " + "\n":
+            n_words = n_words.replace(c, ".")
 
-            no_nums = []
-            for w in n_words:
-                try:
-                    int(w) == int
-                except ValueError:
-                    no_nums.append(w)
-            n_words = no_nums
+        n_words = n_words.lower().strip().split(".")
+        n_words = filter(None, n_words)
 
-            for word in n_words:
-                word.strip()
-                if not word or word == " ":
-                    continue
-                elif len(word) == 1:
-                    continue
-                try:
-                    UserDictionary.objects.get(user=request.user, word=word)
-                except UserDictionary.DoesNotExist:
-                    user_dictionary = UserDictionary(user=request.user, word=word)
-                    user_dictionary.save()
-            return redirect("/my_dictionary")
+        no_nums = []
+        for w in n_words:
+            try:
+                int(w) == int
+            except ValueError:
+                no_nums.append(w)
+        n_words = no_nums
 
-        known_words = set()
-        for user_dictionary in UserDictionary.objects.filter(user=request.user):
-            known_words.add(user_dictionary.word)
+        for word in n_words:
+            word.strip()
+            if not word or word == " ":
+                continue
+            elif len(word) == 1:
+                continue
+            try:
+                UserDictionary.objects.get(user=request.user, word=word)
+            except UserDictionary.DoesNotExist:
+                user_dictionary = UserDictionary(user=request.user, word=word)
+                user_dictionary.save()
+        return redirect("/my_dictionary")
 
-        data = {
-            "known_words": sorted(known_words),
-            "current_page": "my_dictionary"
-        }
-        return render(request, "word_stats/my_dictionary.html", data)
+    known_words = set()
+    for user_dictionary in UserDictionary.objects.filter(user=request.user):
+        known_words.add(user_dictionary.word)
+
+    data = {
+        "known_words": sorted(known_words),
+        "current_page": "my_dictionary"
+    }
+    return render(request, "word_stats/my_dictionary.html", data)
 
 
 #   The same method
