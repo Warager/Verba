9d749279c006da61a27591b7632380f0b92da8a3
Fomin Dmitry <fomin.dritmy@gmail.com>
2014-09-23 18:43:32 +0400

    Checking of migrations

---
+++ verba/word_stats/migrations/0003_auto_20140922_1224.py
@@ -0,0 +1,19 @@
+# -*- coding: utf-8 -*-
+from __future__ import unicode_literals
+
+from django.db import models, migrations
+
+
+class Migration(migrations.Migration):
+
+    dependencies = [
+        ('word_stats', '0002_userdictionary_created'),
+    ]
+
+    operations = [
+        migrations.AlterField(
+            model_name='userdictionary',
+            name='created',
+            field=models.DateTimeField(auto_now_add=True, verbose_name=b'created'),
+        ),
+    ]
