0720fb4ec1f291328306cc732e7217da069119b7
Fomin Dmitry <fomin.dritmy@gmail.com>
2014-10-02 02:22:31 +0400

    form changed to a href
    Maded of reduce csrf error

--- verba/word_stats/static/js/input_form.js
+++ verba/word_stats/static/js/input_form.js
@@ -1,11 +1,5 @@
 $(function (){
-    $(document).on('click', '.process', function () {
-//    var row = $(this).closest('tr');
-//    var newRowWord = row.find('td:nth-child(2)');
-//    var newRow = $('.users-word-table tr:last').clone().show();
-//    var newRowNum = newRow.find('td:nth-child(1)');
-//    var numOfWords = $('.num-of-words');
-
+  $(document).on('click', '.process', function () {
     $.ajax({
       url: "process",
       method: "POST",
@@ -20,18 +14,7 @@ $(function (){
           analysis.html(res.analysis);
           $('.general').hide();
           analysis.show();
-//          newRow.find('td:nth-child(2)').html(newRowWord.text());
-//          newRow.find('td:nth-child(1)').html(parseInt(newRowNum.text()) + 1);
-//          newRow.appendTo('.users-word-table');
-//          numOfWords.html(parseInt(numOfWords.text()) + 1);
-//          row.remove();
-//          $('.countNewWords').each(function (index, td) {
-//            $(td).text(index)
-//          });
         }
-//        else if (res.error == 'error'){
-//          $('#signInOrSignUp').modal("show");
-//        }
       },
       error: ajaxError
     });
